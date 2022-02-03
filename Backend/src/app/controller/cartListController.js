13b83feec09652cae50fa2cd24a3edc1f106d267
BrenneBoss <tchalisantos40@gmail.com>
2022-02-03 17:25:19 -0300

    adicionando a regra de negócio de livros iguais no carrinho

--- Backend/src/app/controller/cartListController.js
+++ Backend/src/app/controller/cartListController.js
@@ -2,38 +2,39 @@ import Livro from '../models/livro';
 import MyCartList from '../models/cartlist';
 
 class MyCartListController {
-    async store(req, res) {
-        const {
-            id_user,
-            id_livro,
-        } = req.body;
-        const myCartList = await MyCartList.create({
-            id_user,
-            id_livro,
-        });
-        return res.json(myCartList);
-      }
+  async store(req, res) {
+    const idExist = await MyCartList.findOne({
+      where: { id_livro: req.body.id_livro },
+    });
 
-    async index(req,res) {
-        const myCartlist = await MyCartList.findAll({
-            where:{
-                id_user: req.params.id
-            },
-            include: [
-                {
-                    model: Livro,
-                    as: "livros",
-                    attributes: [
-                        "nome",
-                        "autor",
-                        "categoria"
-                    ]
-                }
-            ]
-        });
-        return res.json(myCartlist);
+    if (idExist) {
+      return res.status(400).json({ mensagem: 'ID já existe' });
     }
 
+    const { id_user, id_livro } = req.body;
+
+    const myCartList = await MyCartList.create({
+      id_user,
+      id_livro,
+    });
+    return res.json(myCartList);
+  }
+
+  async index(req, res) {
+    const myCartlist = await MyCartList.findAll({
+      where: {
+        id_user: req.params.id,
+      },
+      include: [
+        {
+          model: Livro,
+          as: 'livros',
+          attributes: ['nome', 'autor', 'categoria'],
+        },
+      ],
+    });
+    return res.json(myCartlist);
+  }
 }
 
 export default new MyCartListController();
