2da16f9b92c1ec8ffebf27066b0dad58bd215d3b
Carlos Eduardo <dudu_carlos.eduardo@hotmail.com>
2022-02-07 11:36:00 -0300

    Consertada rota do publicante do livro

--- Frontend/src/pages/cartList/index.js
+++ Frontend/src/pages/cartList/index.js
@@ -62,7 +62,7 @@ function CartList() {
             const { id_livro, nome } = item
             return (
               <div className="containerName">
-                <Link to={`/bookProfile/${id_livro}`}>
+                <Link to={`/contactPage/${id_livro}`}>
                   <p>{nome}</p>
                 </Link>
               </div>
